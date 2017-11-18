a6c28c0fc10edd9514f21b5afc06a10f93fa15eb
wdgreen340 <33792384+wdgreen340@users.noreply.github.com>
2017-11-18 14:51:06 -0800

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,10 +1,1 @@
-node default {
-  file {'/root/README':
-   ensure  => file,
-   content => 'This is a readme',
-   owner   => 'root',
-   }
-   file {'/root/README':
-     owner => 'root',
-   }
-}
+
